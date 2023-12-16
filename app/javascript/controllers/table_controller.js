ca25bc0188d6f79d2f9091ada3d1a92ef7637f6a
Richard Chou <konami99@hotmail.com>
2023-12-16 11:00:23 +1100

    updated controller

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -41,9 +41,24 @@ export default class extends Controller {
       run.textContent = 'RUNNING....';
     }
 
+    const fetchOptions = {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken,
+      },
+    }
+
     for (const command of commandsArray) {
       const commandArray = command.split(' ');
       const header = commandArray[0] //PLACE, MOVE, etc
+      const clonedFetchOptions = { ...fetchOptions };
+      var route;
+
+      await this.sleep(2000)
 
       if (header === 'PLACE') {
         const command = header
@@ -51,78 +66,22 @@ export default class extends Controller {
         const x = params[0]
         const y = params[1]
         const direction = params[2]
+        clonedFetchOptions.body = JSON.stringify({ x: x, y: y, direction: direction });
 
-        await this.sleep(2000)
-        fetch(`/tables/place`, {
-          method: 'POST',
-          mode: 'cors',
-          cache: 'no-cache',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken,
-          },
-          body: JSON.stringify({ x: x, y: y, direction: direction }) 
-        })
-        .then (response => response.text())
-        .then(html => Turbo.renderStreamMessage(html));
+        route = '/tables/place'
       } else if (header === 'MOVE') {
-        await this.sleep(2000)
-        fetch(`/tables/move`, {
-          method: 'POST',
-          mode: 'cors',
-          cache: 'no-cache',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken,
-          }
-        })
-        .then (response => response.text())
-        .then(html => Turbo.renderStreamMessage(html));
+        route = '/tables/move'
       } else if (header === 'LEFT') {
-        await this.sleep(2000)
-        fetch(`/tables/turn_left`, {
-          method: 'POST',
-          mode: 'cors',
-          cache: 'no-cache',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken,
-          }
-        })
-        .then (response => response.text())
-        .then(html => Turbo.renderStreamMessage(html));
+        route = '/tables/turn_left'
       } else if (header === 'RIGHT') {
-        await this.sleep(2000)
-        fetch(`/tables/turn_right`, {
-          method: 'POST',
-          mode: 'cors',
-          cache: 'no-cache',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken,
-          }
-        })
-        .then (response => response.text())
-        .then(html => Turbo.renderStreamMessage(html));
+        route = '/tables/turn_right'
       } else if (header === 'REPORT') {
-        await this.sleep(2000)
-        fetch(`/tables/report`, {
-          method: 'POST',
-          mode: 'cors',
-          cache: 'no-cache',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken,
-          }
-        })
+        route = '/tables/report'
+      }
+
+      fetch(route, clonedFetchOptions)
         .then (response => response.text())
         .then(html => Turbo.renderStreamMessage(html));
-      }
     }
     
     if (run) {
